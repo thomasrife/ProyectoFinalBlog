c986b9e4936e46aa1d35ded488df8bd3e1b21e3f
Thomas Rife <thomi.rife@gmail.com>
2022-06-04 13:55:20 -0300

    Sube Blogger. Faltan retoques visuales.

---
+++ blogger/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.urls import path
+from blogger import views
+
+urlpatterns = [
+    path("crear/", views.SignUpView.as_view(), name ="blogger_signup"),
+    path("profile/<pk>/", views.BloggerProfile.as_view(), name ="blogger_profile"),
+    path("editar/<pk>/", views.BloggerUpdate.as_view(), name ="blogger_edit"),
+]
